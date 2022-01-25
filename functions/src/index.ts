76cb58d1224ac015b6b6e243942c948759f969ee
Pascal Birchler <pascalb@google.com>
2022-01-25 12:20:47 +0100

    Set latest version in config

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -15,77 +15,42 @@
  */
 
 import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
-
-admin.initializeApp();
 
 // Use global variables to reuse objects in future invocations
 // See https://firebase.google.com/docs/functions/tips#use_global_variables_to_reuse_objects_in_future_invocations
 const BUCKET_NAME = 'web-stories-wp-cdn-assets';
 const BUCKET_URL = `https://storage.googleapis.com/${BUCKET_NAME}`;
-const bucket = admin.storage().bucket(BUCKET_NAME);
-
-let availableVersions: string[] = [];
 
 /**
- * Handle incoming requests to wp.stories.google/static/:version/path/to/file.jpg
+ * Handle incoming requests to wp.stories.google/static/main/:path
  *
- * If :version is "main", the latest version is retrieved and the user redirected
- * to that version.
- *
- * If :version is numeric, the user is redirected to the version's bucket unchanged.
+ * Redirects them to the GCP bucket for the latest version,
+ * which is set via `firebase functions:config:set`.
  *
  * @todo Consider runWith() with minInstances === 1 to limit number of cold starts.
  * See https://firebase.google.com/docs/functions/manage-functions#min-max-instances
  */
 export const handleCdnRequests = functions.https.onRequest(
-  async (request, response) => {
+  (request, response) => {
     functions.logger.info('Serving for requested path', request.path);
 
     // "/static/123/images/path/to/image.png" => "123", "images/path/to/image.png".
-    const match = request.path.match(/static\/(main|\d+)\/(.*)/);
+    const match = request.path.match(/static\/main\/(.*)/);
 
     if (!match) {
       response.status(404).send();
       return;
     }
 
-    const [, version, fileName] = match;
+    const [, fileName] = match;
 
-    if ('main' !== version) {
-      // TODO: Just achieve this with a regex redirect.
-      response.redirect(`${BUCKET_URL}/static/${version}/${fileName}`);
-      return;
-    }
+    const latestVersion = functions.config().assets?.versions?.latest;
 
-    if (!availableVersions.length) {
-      const topLevelBucketFiles: String[] = (
-        await bucket.getFiles({
-          autoPaginate: false,
-          delimiter: '/',
-        })
-      )[2].prefixes;
+    functions.logger.info('Latest version from config:', latestVersion);
 
-      if (!Array.isArray(topLevelBucketFiles)) {
-        response.status(404).send();
-        return;
-      }
-
-      availableVersions = topLevelBucketFiles
-        .map((name) => name.replace('/', ''))
-        .sort((a, b) => Number(a) - Number(b));
-    }
-
-    functions.logger.info('Available versions: ', availableVersions.join(', '));
-
-    if (availableVersions.length) {
-      const latestVersion = availableVersions.pop();
-      functions.logger.info('Latest version: ', latestVersion);
-
+    if (latestVersion) {
       response.redirect(`${BUCKET_URL}/static/${latestVersion}/${fileName}`);
+      return;
     }
 
     response.status(404).send();
